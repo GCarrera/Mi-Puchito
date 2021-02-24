c0793d213f0a3c39c941a636c3fec6d67327390d
el4l3x <davidlopez.fya@gmail.com>
2021-02-24 13:25:30 -0400

    permisos de usuario

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -11,7 +11,7 @@ class InventoryController extends Controller
 
     public function __construct()
     {
-        $this->middleware('admin');
+        $this->middleware('almacen');
     }
 
     /**
