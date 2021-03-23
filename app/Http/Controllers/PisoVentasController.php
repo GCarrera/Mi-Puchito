1ce2ac8c853b7773dc8ad634349e4fa540bf76ed
el4l3x <davidlopez.fya@gmail.com>
2021-03-23 15:46:10 -0400

    cuatro

--- app/Http/Controllers/PisoVentasController.php
+++ app/Http/Controllers/PisoVentasController.php
@@ -413,7 +413,7 @@ class PisoVentasController extends Controller
       $solicitudes = $request->data;
 
       foreach ($solicitudes as $key => $value) {
-        return $key->nombre;
+        return $value['nombre'];
         $solicitud = new Solicitud();
 
     		$solicitud->nombre = $value->nombre;
