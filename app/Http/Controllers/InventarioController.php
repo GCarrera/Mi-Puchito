02a702ec749c2dc6ec445f8d8daea5c5815a7696
el4l3x <davidlopez.fya@gmail.com>
2021-08-12 12:58:21 -0400

    rt

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -101,10 +101,8 @@ class InventarioController extends Controller
 		foreach ($request->data as $key => $value) {
             //return response()->json($valor);
             $product = Product::where('inventory_id', $value['inventory_id']);
-            if (isset($product->id)) {
-                if ($product->retail_total_price != $value['retail_total_price']) {
-                    array_push($data, $product);
-                }
+            if ($product->retail_total_price != $value['retail_total_price']) {
+                array_push($data, $product);
             }
 		}
 
