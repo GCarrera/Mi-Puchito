798245572372f646107d8749d2d5d8b471c6936b
el4l3x <davidlopez.fya@gmail.com>
2021-10-23 16:50:26 -0400

    soft delete products

--- app/Product.php
+++ app/Product.php
@@ -4,9 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Gloudemans\Shoppingcart\Contracts\Buyable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model implements Buyable 
 {
+
+    use SoftDeletes;
+    
     protected $fillable = ['cost', 'iva_percent', 'retail_margin_gain', 'retail_pvp', 'retail_total_price', 'retail_iva_amount', 'image', 'wholesale_margin_gain', 'wholesale_pvp', 'wholesale_total_individual_price', 'wholesale_total_packet_price', 'wholesale_iva_amount', 'inventory_id'];
 
     public function inventory()
